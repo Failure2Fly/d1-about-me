418595863ab73506fa3a0ffa330c85d74d8b32e5
Devin Blankenship <failure2fly@gmail.com>
2017-02-13 15:27:47 -0500

    about me

---
+++ js/about-me.js
@@ -0,0 +1,18 @@
+var firstName = 'Devin';
+var lastName = 'Blankenship';
+var fullName = firstName + ' ' + lastName;
+var email = 'failure2fly@gmail.com';
+var grewUpInCity = false;
+var yearsInCollege = 0;
+var numberPets = 0;
+var hoursOfSleep = 6.5;
+var favoriteGenreMusic = 'Pop Punk';
+var favoriteShowsMovies = ['The Simpsons', 'Saturday Night Live', 'The Office', 'Californication', 'Eternal Sunshine of the Spotless Mind', 'Donnie Darko', 'Rushmore', 'The Princess Bride'];
+var jobTitleFiveYears;
+var lifeWithoutJs = 'null';
+var graduationDate = new Date();
+graduationDate.setFullYear(2006);
+graduationDate = graduationDate.getFullYear();
+
+
+console.log(firstName, lastName, fullName, email, grewUpInCity, yearsInCollege, numberPets, hoursOfSleep, favoriteGenreMusic, favoriteShowsMovies, jobTitleFiveYears, lifeWithoutJs, graduationDate);
